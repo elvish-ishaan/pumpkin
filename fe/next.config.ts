5c22e6d9fee93daf44dd519b7dfcd2e9bd2161a8
elvish-ishaan <ishaanelvish@gmail.com>
2025-11-09 12:46:32 +0530

    fix: fixed nextconfig for image rendering

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://storage.googleapis.com/pumpkin-bucket/**')],
+    remotePatterns: [new URL('https://storage.googleapis.com/pumpkin-bucket-v2/**')],
   },
 };
 
