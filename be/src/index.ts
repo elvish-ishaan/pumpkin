30ded5cc49fbb82338aa5f68786fd0bb280b0577
elvish-ishaan <ishaanelvish@gmail.com>
2025-12-25 17:55:50 +0530

    feat: aaded a scalling route(tmp)

--- be/src/index.ts
+++ be/src/index.ts
@@ -526,6 +526,30 @@ app.get("/", (req: Request, res: Response) => {
   })
 })
 
+app.get("/scale", (req, res) => {
+  try {
+    function calculatePrimes(limit: number) {
+  const primes = [];
+  for (let i = 2; i <= limit; i++) {
+    let isPrime = true;
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) primes.push(i);
+  }
+  return primes;
+}
+
+// Running this with a high number will spike CPU usage
+console.log(calculatePrimes(10000000).length);
+  } catch (error) {
+    console.log(error,'error in scalling')
+  }
+})
+
 app.listen(8080, () => {
   console.log('Server is running on port 8080')
 })
